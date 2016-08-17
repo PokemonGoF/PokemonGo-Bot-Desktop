3b9ca1327d67defd346a02267d7e81fad352eebf
Guillaume Migeon <guillaume.migeon.ext@boursorama.fr>
2016-08-17 10:35:23 +0200

    Code reformat

--- app/electron.js
+++ app/electron.js
@@ -1,66 +1,66 @@
-'use strict'
+'use strict';
 
-const electron = require('electron')
-const path = require('path')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron      = require('electron'),
+      path          = require('path'),
+      app           = electron.app,
+      BrowserWindow = electron.BrowserWindow;
 
-let mainWindow
-let config = {}
+let mainWindow,
+    config = {};
 
 if (process.env.NODE_ENV === 'development') {
-  config = require('../config')
-  config.url = `http://localhost:${config.port}`
+    config     = require('../config');
+    config.url = `http://localhost:${config.port}`;
 } else {
-  config.devtron = false
-  config.url = `file://${__dirname}/dist/index.html`
+    config.devtron = false;
+    config.url     = `file://${__dirname}/dist/index.html`;
 }
 
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
-  })
+function createWindow() {
+    /**
+     * Initial window options
+     */
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+    });
 
 
-  global.appRoot = path.resolve(__dirname);
-  global.botPath = path.join(global.appRoot, 'gofbot')
-  if (process.env.NODE_ENV === 'development') {
-      global.botPath = path.join(global.appRoot, 'dist/gofbot')
-  }
+    global.appRoot = path.resolve(__dirname);
+    global.botPath = path.join(global.appRoot, 'gofbot');
+    if (process.env.NODE_ENV === 'development') {
+        global.botPath = path.join(global.appRoot, 'dist/gofbot')
+    }
 
-  mainWindow.loadURL(config.url);
+    mainWindow.loadURL(config.url);
 
-  if (process.env.NODE_ENV === 'development') {
-    BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'))
+    if (process.env.NODE_ENV === 'development') {
+        BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'));
 
-    let installExtension = require('electron-devtools-installer')
+        let installExtension = require('electron-devtools-installer');
 
-    installExtension.default(installExtension.VUEJS_DEVTOOLS)
-      .then((name) => mainWindow.webContents.openDevTools())
-      .catch((err) => console.log('An error occurred: ', err))
-  }
+        installExtension.default(installExtension.VUEJS_DEVTOOLS)
+            .then((name) => mainWindow.webContents.openDevTools())
+            .catch((err) => console.log('An error occurred: ', err))
+    }
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    });
 
-  console.log('mainWindow opened')
+    console.log('mainWindow opened')
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
 
 app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
